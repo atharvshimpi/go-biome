3decf244012bea5c05637a7a0a1d79068b298173
= <=>
2022-07-21 22:00:55 +0530

    init

---
+++ public/serviceworker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "version-1"
+// eslint-disable-next-line
+const urlsToCache = ['index.html', 'offline.html']
+
+const self = this
+
+// Install SW
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                // eslint-disable-next-line
+                console.log("Opened Cache1")
+                return cache.addAll(urlsToCache)
+            })
+    )
+})
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    // eslint-disable-next-line
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+})
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = []
+    cacheWhiteList.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhiteList.includes(cacheName)) {
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    )
+})

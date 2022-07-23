ee3841ec24380bd8969d89564b976e9531b92aaa
= <=>
2022-07-23 15:21:02 +0530

    questions

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react"
+import TimePicker from "react-time-picker"
 // eslint-disable-next-line
 import { data, dataElements } from "./questionsData"
 
 import "./questions.css"
 
+const answerFormat = { q1: "", q2: "", q3: "08:00", q4: "" }
+
 const Questions = () => {
     // eslint-disable-next-line
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
     const [isNextClicked, setIsNextClicked] = useState(false)
-    // eslint-disable-next-line
-    console.log(data)
+    const [answers, setAnswers] = useState(answerFormat)
+    const [timeValue, onTimeValueChange] = useState("10:00")
+    
+    const handleClick = () => {
+        // eslint-disable-next-line
+        console.log(answers)
+    }
 
     return (
         <>
@@ -26,25 +34,59 @@ const Questions = () => {
                                 </span>
                             </div>
                             <div className="questions-about">
-                                <div></div>
+                                {/* input */}
+                                {key === 0 || key === 1 ? (
+                                    <div className="answer-text-input">
+                                        <input
+                                            className="biome-name"
+                                            type="text"
+                                            onChange={(e) => setAnswers({ ...answers, [e.target.id]: e.target.value })}
+                                            placeholder={`Enter ${item.question.split(" ")[2]} biome name`}
+                                            value={key === 0 ? answers.q1 : answers.q2}
+                                            id={key === 0 ? "q1" : "q2"}
+                                        />
+                                    </div>
+                                ) : key === 2 ? (
+                                    <div className="answer-time-input">
+                                        <span>Waking Time : </span>
+                                        <TimePicker
+                                            onChange={onTimeValueChange} 
+                                            value={timeValue}
+                                        />
+                                    </div>
+                                ) : (
+                                    <div>4</div>
+                                )}
                             </div>
                             <div className="questions-btn-container">
-                                <a
-                                    className="questions-btn-a"
-                                    href={`#${key - 1}`}
-                                >
-                                    <button className="questions-btn">
-                                        Back
-                                    </button>
-                                </a>
-                                <a
-                                    className="questions-btn-a"
-                                    href={`#${key + 1}`}
-                                >
-                                    <button className="questions-btn">
-                                        Next
-                                    </button>
-                                </a>
+                                {key !== 0 && 
+                                    <a
+                                        className="questions-btn-a"
+                                        href={`#${key - 1}`}
+                                    >
+                                        <button className="questions-btn">
+                                            Back
+                                        </button>
+                                    </a>
+                                }
+                                {key === 3 ? (
+                                    <a
+                                        className="questions-btn-a"
+                                    >
+                                        <button onClick={handleClick} className="questions-btn">
+                                            Submit
+                                        </button>
+                                    </a>
+                                ) : (
+                                    <a
+                                        className="questions-btn-a"
+                                        href={`#${key + 1}`}
+                                    >
+                                        <button className="questions-btn">
+                                            Next
+                                        </button>
+                                    </a>
+                                )}
                             </div>
                         </div>
                     )

25d28b6bf4b0bc8c25099265e3da1d370aae7d28
= <=>
2022-08-02 01:47:47 +0530

    haptic vibration updated

--- src/components/settings/general.js
+++ src/components/settings/general.js
@@ -1,13 +1,28 @@
-import React from "react"
+import React , { useState } from "react"
+import { Switch } from "@headlessui/react"
 
 import { settingDetails } from "./settingsData"
 
 import { IoIosArrowForward } from "react-icons/io"
 // eslint-disable-next-line
 const GeneralSettings = ({ setOpen, setSettingId }) => {
+    const [pref, setPref] = useState(JSON.parse(localStorage.getItem("preferences")))
+    const [enabled, setEnabled] = useState(pref.vibrate)
+
     const handleClick = (id) => {
         setSettingId(id)
         setOpen(true)
+    }
+
+    // messed up code - do it properly
+    const handleVibrate = () => {
+        setEnabled(!enabled)
+        setPref({ ...pref, vibrate: enabled })
+        localStorage.setItem("preferences", JSON.stringify({ ...pref, vibrate: !enabled }))
+
+        if(!enabled) {
+            window.navigator.vibrate(500)
+        }
     }
 
     return (
@@ -22,7 +37,19 @@ const GeneralSettings = ({ setOpen, setSettingId }) => {
                                     {obj.icon}
                                     <p>{obj.title}</p>
                                 </div>
-                                <IoIosArrowForward className="icon" onClick={() => handleClick(obj.id)}/>
+                                {obj.id === 6 ? 
+                                    <Switch
+                                        checked={enabled}
+                                        onChange={handleVibrate}
+                                        className={`${enabled ? "bg-yellow-600" : "bg-[#ffbc58]"} relative inline-flex h-6 w-11 items-center rounded-full`}
+                                    >
+                                        <span
+                                            className={`${enabled ? "translate-x-6" : "translate-x-1"} inline-block h-4 w-4 transform transition ease-in-out duration-200 rounded-full bg-white`}
+                                        />
+                                    </Switch>
+                                    :
+                                    <IoIosArrowForward className="icon" onClick={() => handleClick(obj.id)}/>
+                                }
                             </div>  
                         )
                     })}

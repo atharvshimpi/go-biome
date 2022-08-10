dcf43c1280dd3b5a00ff1f04ce33230946352cf1
= <=>
2022-08-10 19:42:25 +0530

    user card template

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import Dashboard from "./components/dashboard/dashboard"
 import Questions from "./components/questions/questions"
 import Login from "./components/login/login"
 import Settings from "./components/settings/settings"
-import Gamemap from "./components/gameboard/gamemap"
+import UserCard from "./components/modals/userCard"
 
 const App = () => {
     const [authenticated, setAuthenticated] = useState(null)
@@ -44,7 +44,7 @@ const App = () => {
                         }
                     />
                     <Route path="/settings" element={<Settings />} />
-                    <Route path="/map" element={<Gamemap />} />
+                    <Route path="/card" element={<UserCard />} />
                 </Routes>
             </AnimatePresence>
         </Router>
---
+++ src/utils/exportXLSX.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ExportXLSX = () => {
+    return (
+        <div>ExportXLSX</div>
+    )
+}
+
+export default ExportXLSX

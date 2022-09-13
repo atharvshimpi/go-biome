40500f96f13c6c754669ac032fe49b62da127c24
= <=>
2022-09-13 22:49:22 +0530

    firebase sign in pop up update 1.0

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -13,12 +13,12 @@ const AuthContext = createContext()
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     
-    const googleSignIn = () => {
+    const googleSignIn = async () => {
         const provider = new GoogleAuthProvider()
         provider.setCustomParameters({
             prompt: "select_account"
         })
-        signInWithPopup(auth, provider)
+        await signInWithPopup(auth, provider)
     }
 
     const logOut = (navigate) => {

919d5aa5994baaf30afa6ca3abf4828aa09c9348
atharvshimpi <atharvashimpi@gmail.com>
2023-01-04 22:25:49 +0530

    user card stack

--- src/components/modals/swiperModal.js
+++ src/components/modals/swiperModal.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { EffectCards } from "swiper"
-import ReactCardFlip from "react-card-flip"
 
 import select from "../../assets/sounds/UI/Proceed.mp3"
 import Environment from "../../assets/images/category/environment.svg"
@@ -50,7 +49,6 @@ const SwiperModal = ({ gender, activityUserCards, cardCategory }) => {
             className="mySwiper"
         >
             {activityUserCards.map((obj, key) => {
-                const [isCardFlipped, setIsCardFlipped] = useState(false)
                 const [cardLoading, setCardLoading] = useState(true)
                 const [iconImage, setIconImage] = useState(null)
                 const multiGenderAvaialble =
@@ -70,147 +68,66 @@ const SwiperModal = ({ gender, activityUserCards, cardCategory }) => {
 
                 return (
                     <SwiperSlide key={key}>
-                        <ReactCardFlip
-                            isFlipped={isCardFlipped}
-                            flipDirection="horizontal"
+                        <div
+                            className="activity-cardstack-card-container"
+                            style={{ backgroundColor: "#ffffff" }}
                         >
-                            {/* Front Side */}
-                            <div
-                                className="activity-cardstack-card-container"
-                                style={{ backgroundColor: "#ffffff" }}
-                            >
-                                {cardLoading && (
-                                    <Box
-                                        sx={{
-                                            display: "flex",
-                                            justifyContent: "center",
-                                            fontSize: "0.8rem",
-                                            position: "relative",
-                                            left: cardLoading ? 0 : 0,
+                            {cardLoading && (
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        fontSize: "0.8rem",
+                                        position: "relative",
+                                        left: cardLoading ? 0 : 0,
+                                    }}
+                                >
+                                    <CircularProgress
+                                        style={{
+                                            width: "30px",
+                                            height: "30px",
                                         }}
-                                    >
-                                        <CircularProgress
-                                            style={{
-                                                width: "30px",
-                                                height: "30px",
-                                            }}
-                                        />
-                                    </Box>
-                                )}
-                                {/* <h1 className="swiper-modal-activity-title">{obj.task}</h1> */}
-                                <div className="activity-cardstack-location">
-                                    <GoLocation /><p>{obj.location}</p>
+                                    />
+                                </Box>
+                            )}
+                            <div className="activity-cardstack-category-container">
+                                <img src={require(`../../assets/images/category/${obj.category}.svg`)} style={{ backgroundColor: categoryTags[obj.categoryId].color }} alt={obj.task} />
+                            </div>
+                            <h1 className="swiper-modal-activity-title">{obj.task}</h1>
+                            <div className="activity-cardstack-location">
+                                <GoLocation /><p>{obj.location}</p>
+                            </div>
+                            <img
+                                style={{ opacity: cardLoading ? 0 : 1 }}
+                                onLoad={imageLoaded}
+                                // onClick={() =>
+                                //     setIsCardFlipped(!isCardFlipped)
+                                // }
+                                src={obj.icon.length > 4 ? obj.icon : require(`../../assets/images/cards/${
+                                    obj.category
+                                }/${
+                                    multiGenderAvaialble
+                                        ? `${obj.icon}${gender}`
+                                        : obj.icon
+                                }.png`)}
+                                alt={obj.icon}
+                            />
+                            <div className="activity-cardstack-card-description-container" style={{
+                                backgroundColor:
+                                categoryTags[cardCategory != null ? cardCategory : obj.categoryId].color,
+                            }}>
+                                <div className="activity-cardstack-card-description">
+                                    <p className="cardstack-card-text"><strong>Description</strong>: {obj.description}</p>
                                 </div>
-                                <img
-                                    style={{ opacity: cardLoading ? 0 : 1 }}
-                                    onLoad={imageLoaded}
-                                    // onClick={() =>
-                                    //     setIsCardFlipped(!isCardFlipped)
-                                    // }
-                                    src={obj.icon.length > 4 ? obj.icon : require(`../../assets/images/cards/${
-                                        obj.category
-                                    }/${
-                                        multiGenderAvaialble
-                                            ? `${obj.icon}${gender}`
-                                            : obj.icon
-                                    }.png`)}
-                                    alt={obj.icon}
-                                />
-                                <div className="activity-cardstack-card-description-container" style={{
-                                    backgroundColor:
-                                    categoryTags[cardCategory != null ? cardCategory : obj.categoryId].color,
-                                }}>
-                                    <div className="activity-cardstack-card-description">
-                                        <p className=""><strong>Description</strong>: {obj.description}</p>
-                                    </div>
-                                    <div className="activity-cardstack-date-container">
-                                        <AiOutlineCalendar />
-                                        <div><p className="">Mon, 22 Aug 2022</p></div>
-                                    </div>
-                                    <div onClick={() => {}} className="repeat-button">
-                                        Repeat Activity
-                                    </div>
+                                <div className="activity-cardstack-date-container">
+                                    <AiOutlineCalendar />
+                                    <div><p className="cardstack-card-date">Mon, 22 Aug 2022</p></div>
+                                </div>
+                                <div onClick={() => {}} className="repeat-button">
+                                    Repeat Activity
                                 </div>
                             </div>
-                            {/* Back Side */}
-                            <div
-                                className="card-container"
-                                onClick={() => setIsCardFlipped(!isCardFlipped)}
-                                style={{
-                                    backgroundColor:
-                                        categoryTags[cardCategory != null ? cardCategory : obj.categoryId].color,
-                                }}
-                            >
-                                <div className="card-content">
-                                    <div className="swiper-modal-inner">
-                                        <div className="swiper-modal-activity">
-                                            <div
-                                                style={{ backgroundColor: categoryTags[obj.categoryId].avatarColor }}
-                                                className="cards"
-                                            >
-                                                <img src={iconImage} className="swiper-modal-image"/>
-                                            </div>
-                                            <h1 className="swiper-modal-activity-title">{obj.task}</h1>
-                                            <h4 className="swiper-modal-activity-title1">
-                                                <GoLocation /> {obj.location}
-                                            </h4>
-                                        </div>
-                                        <img src="" className="swiper-modal-image-main"/>
-                                    </div>
-
-                                    <div className="swiper-modal-description">
-                                        <p className="swiper-modal-main-text">Description: {obj.description}</p>
-                                    </div>
-
-                                    <div className="swiper-modal-date">
-                                        <AiOutlineCalendar />
-                                        <div><p className="swiper-modal-main-text-date">Mon, 22 Aug 2022</p></div>
-                                    </div>
-
-                                    <div className="swiper-modal-repeat">
-                                        <p className="swiper-modal-main-text1">REPEAT ACTIVITY</p>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <div
-                                className="card-container"
-                                onClick={() => setIsCardFlipped(!isCardFlipped)}
-                                style={{
-                                    backgroundColor:
-                                        categoryTags[cardCategory != null ? cardCategory : obj.categoryId].color,
-                                }}
-                            >
-                                <div className="card-content">
-                                    <p className="header">{obj.task}</p>
-                                    <div className="btngrp">
-                                        <button
-                                            className="btn"
-                                            onClick={(e) => handleClick(e, obj)}
-                                        >
-                                            {cardLoading ? (
-                                                <Box
-                                                    sx={{
-                                                        display: "flex",
-                                                        justifyContent:
-                                                            "center",
-                                                        fontSize: "0.8rem",
-                                                    }}
-                                                >
-                                                    <CircularProgress
-                                                        style={{
-                                                            width: "30px",
-                                                            height: "30px",
-                                                        }}
-                                                    />
-                                                </Box>
-                                            ) : (
-                                                "Start now"
-                                            )}
-                                        </button>
-                                    </div>
-                                </div>
-                            </div> */}
-                        </ReactCardFlip>
+                        </div>
                     </SwiperSlide>
                 )
             })}
